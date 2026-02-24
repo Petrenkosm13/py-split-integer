07c3e4b137681d2ad614428029fbd3bee738dc35
Serhii <petrenkosm13@gmail.com>
2026-02-24 20:29:49 +0100

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,9 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(17, 4)) == 17
+    result = split_integer(17, 4)
+    assert result == [4, 4, 4, 5]
+    assert sum(result) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -19,3 +21,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(3, 4) == [0, 1, 1, 1]
+
+
+def test_split_integer_example_32_6() -> None:
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
